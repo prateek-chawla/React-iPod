8543d7e65a24989288bf022e6e86546fd79220f5
Prateek Chawla <prateek.chawla98@gmail.com>
2020-08-06 20:41:49 +0530

    Updates

---
+++ precache-manifest.a7e5d6e1af5da73ca4e282e09d88a5b3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb02a6b32badd787c5a3470a22f7468",
+    "url": "/React-iPod/index.html"
+  },
+  {
+    "revision": "906d696576d2afee190b",
+    "url": "/React-iPod/static/css/main.de03b178.chunk.css"
+  },
+  {
+    "revision": "02bf2d04d26cb9630017",
+    "url": "/React-iPod/static/js/2.e868110b.chunk.js"
+  },
+  {
+    "revision": "0395921f8b56d12a4870f8ea70bf3c15",
+    "url": "/React-iPod/static/js/2.e868110b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "906d696576d2afee190b",
+    "url": "/React-iPod/static/js/main.633d9aea.chunk.js"
+  },
+  {
+    "revision": "0f9441e769e2e69cb135",
+    "url": "/React-iPod/static/js/runtime-main.bd5aab2d.js"
+  },
+  {
+    "revision": "839a2c83ba83f72345e175c970c6d4a3",
+    "url": "/React-iPod/static/media/Games.839a2c83.png"
+  },
+  {
+    "revision": "2d2f3b57259b88c219ae39e437a54bfc",
+    "url": "/React-iPod/static/media/Music.2d2f3b57.png"
+  },
+  {
+    "revision": "d5e51f97b234ddcbcf6f7bfe979a97da",
+    "url": "/React-iPod/static/media/Settings.d5e51f97.png"
+  },
+  {
+    "revision": "1a178d65b5e69c30e302e65087443c76",
+    "url": "/React-iPod/static/media/Weather.1a178d65.png"
+  },
+  {
+    "revision": "2ceadd10ca85adca14362906f2379fdc",
+    "url": "/React-iPod/static/media/apple-font.2ceadd10.otf"
+  },
+  {
+    "revision": "009a001a8043c5c684d8c78b49bb4fbb",
+    "url": "/React-iPod/static/media/bird.009a001a.png"
+  }
+]);
