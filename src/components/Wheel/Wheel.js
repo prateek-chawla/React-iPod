6ec5711e5aef044c1e5cccbe6bad73b35011d607
Prateek Chawla <prateek.chawla98@gmail.com>
2020-08-06 22:32:48 +0530

    Change to Function component

--- src/components/Wheel/Wheel.js
+++ src/components/Wheel/Wheel.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 
 import "./Wheel.css";
@@ -6,53 +6,40 @@ import setUpWheelEvents from "./wheelEvents";
 
 import * as actions from "../../store/actions";
 
-class Wheel extends Component {
-	constructor(props) {
-		super(props);
-		// DOM Reference to Wheel
-		this.wheelRef = React.createRef();
-	}
-	componentDidMount() {
-		// Separate Wheel Events Handler
-		setUpWheelEvents(
-			this.wheelRef.current,
-			this.props.onMoveForward,
-			this.props.onMoveBackward
-		);
-	}
-	render() {
-		return (
-			<div id="wheel" ref={this.wheelRef}>
-				<div onClick={this.props.onMenuBtnPressed} className="wheel__item menu-btn">
-					MENU
-				</div>
-				<div
-					onClick={this.props.onFwdBtnPressed}
-					className="wheel__item seekfwd-btn"
-				>
-					<i className="fas fa-forward"></i>
-				</div>
-				<div
-					onClick={this.props.onBwdBtnPressed}
-					className="wheel__item seekbwd-btn"
-				>
-					<i className="fas fa-backward"></i>
-				</div>
-				<div
-					onClick={this.props.onPlayPauseBtnPressed}
-					className="wheel__item play-pause-btn"
-				>
-					<i className="fas fa-play"></i>
-					<i className="fas fa-pause"></i>
-				</div>
-				<div
-					onClick={this.props.onSelectBtnPressed}
-					className="wheel__item select-btn"
-				></div>
+const Wheel = props => {
+	// 		// DOM Reference to Wheel
+	const wheelRef = useRef(null);
+
+	useEffect(() => {
+		// 		// Separate Wheel Events Handler
+		setUpWheelEvents(wheelRef.current, props.onMoveForward, props.onMoveBackward);
+	}, [setUpWheelEvents]);
+
+	return (
+		<div id="wheel" ref={wheelRef}>
+			<div onClick={props.onMenuBtnPressed} className="wheel__item menu-btn">
+				MENU
 			</div>
-		);
-	}
-}
+			<div onClick={props.onFwdBtnPressed} className="wheel__item seekfwd-btn">
+				<i className="fas fa-forward"></i>
+			</div>
+			<div onClick={props.onBwdBtnPressed} className="wheel__item seekbwd-btn">
+				<i className="fas fa-backward"></i>
+			</div>
+			<div
+				onClick={props.onPlayPauseBtnPressed}
+				className="wheel__item play-pause-btn"
+			>
+				<i className="fas fa-play"></i>
+				<i className="fas fa-pause"></i>
+			</div>
+			<div
+				onClick={props.onSelectBtnPressed}
+				className="wheel__item select-btn"
+			></div>
+		</div>
+	);
+};
 
 const mapDispatchToProps = dispatch => {
 	return {
