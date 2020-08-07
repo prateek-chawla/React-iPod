20ae3f840684c35e86e1139e951a99a1449a6110
Prateek Chawla <prateek.chawla98@gmail.com>
2020-08-08 00:39:05 +0530

    Updated Music Component

--- src/components/DisplayItems/Music/NowPlaying/NowPlaying.js
+++ src/components/DisplayItems/Music/NowPlaying/NowPlaying.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { connect } from "react-redux";
 
+import { formatTime } from "../../../../utils";
+
 import * as actions from "../../../../store/actions";
 
 import "./NowPlaying.css";
@@ -10,6 +12,20 @@ const NowPlaying = props => {
 	const { resetNowPlaying, track } = props;
 
 	const [isPlaying, setIsPlaying] = useState(false);
+	const [currentTime, setCurrentTime] = useState("00:00");
+
+	useEffect(() => {
+		// Update current playback time
+		const updateCurrentTime = setInterval(() => {
+			setCurrentTime(trackRef.current.currentTime);
+			if (trackRef.current.currentTime === trackRef.current.duration)
+				setIsPlaying(false);
+		}, 500);
+
+		return () => {
+			clearInterval(updateCurrentTime);
+		};
+	}, []);
 
 	useEffect(() => {
 		// Reset Current Track when exiting
@@ -36,9 +52,17 @@ const NowPlaying = props => {
 	const playIcon = <i className="fas fa-play"></i>;
 	const pauseIcon = <i className="fas fa-pause"></i>;
 
+	// Set Play Time
+	let playTime = null;
+	if (currentTime && trackRef.current && !isNaN(trackRef.current.duration))
+		playTime = (
+			<div className="playTime">
+				{formatTime(currentTime)} / {formatTime(trackRef.current.duration)}{" "}
+			</div>
+		);
+
 	return (
 		<div className="NowPlaying">
-
 			<div className="nowPlayingImg">
 				<img className="nowPlayingImg" src={props.track.imageUrl} alt="Album Art" />
 				<div className="nowPlayingIcon">{isPlaying ? pauseIcon : playIcon}</div>
@@ -46,6 +70,7 @@ const NowPlaying = props => {
 
 			<div className="nowPlayingName">{props.track.name}</div>
 			<div className="nowPlayingArtist">{props.track.artist}</div>
+			{playTime}
 			<audio ref={trackRef} src={props.track.songUrl} />
 		</div>
 	);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,18 @@
 export const convertToCelsius = kelvinTemp => {
 	return parseFloat((kelvinTemp - 273.15).toFixed(1)) + "°C";
 };
+
+// Format time in hh:mm:ss format
+export const formatTime = time => {
+	const timeInSeconds = parseInt(time);
+	const ss = addPadding(parseInt(timeInSeconds % 60));
+	const mins = parseInt(timeInSeconds / 60);
+	const mm = addPadding(parseInt(mins % 60));
+	const hours = parseInt(mins / 60);
+	const hh = addPadding(parseInt(hours % 60));
+	return hh > 0 ? `${hh}:${mm}:${ss}` : `${mm}:${ss}`;
+};
+
+const addPadding = num => {
+	return num < 10 ? "0" + num : num;
+};
